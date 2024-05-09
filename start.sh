e1057822c7fdbc99a9f8f05474e3e19f037a0576
Michael-Bill <biloshitskiy_m@mail.ru>
2024-05-09 22:28:26 +0300

    feat: jvisualvm

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ docker rmi -f opi-lab4-jsf-web-lab3-application
 docker-compose up -d
 open http://localhost:8080/laba3_web
 jconsole -J-Djava.class.path=/Users/mihailbilosickij/Documents/Programs/jboss-cli-client.jar > /dev/null &
+jvisualvm --cp:a /Users/mihailbilosickij/Documents/Programs/jboss-cli-client.jar > /dev/null &
 # for connect: service:jmx:http-remoting-jmx://localhost:9990
 # user: user1, password: Qwerty123
