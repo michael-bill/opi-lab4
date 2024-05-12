edc5923b658d807088256f6c1708b7a84d8b4240
Michael-Bill <biloshitskiy_m@mail.ru>
2024-05-12 20:53:26 +0300

    fix: start sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ docker rm -f jsf-web-lab3-application
 docker rmi -f opi-lab4-jsf-web-lab3-application
 docker-compose up -d
 open http://localhost:8080/laba3_web
-jconsole -J-Djava.class.path=/Users/mihailbilosickij/Documents/Programs/jboss-cli-client.jar > /dev/null &
-jvisualvm --cp:a /Users/mihailbilosickij/Documents/Programs/jboss-cli-client.jar > /dev/null &
+jconsole -J-Djava.class.path=/Users/mihailbilosickij/Documents/Programs/jboss-cli-client.jar > /dev/null 2>&1 &
+jvisualvm --cp:a /Users/mihailbilosickij/Documents/Programs/jboss-cli-client.jar > /dev/null 2>&1 &
 # for connect: service:jmx:http-remoting-jmx://localhost:9990
 # user: user1, password: Qwerty123
