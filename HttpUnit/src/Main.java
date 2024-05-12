d18fc2f974a4b262a49784b8c3096a771060591e
Michael-Bill <biloshitskiy_m@mail.ru>
2024-05-12 19:56:55 +0300

    fix: speeding up the program

--- HttpUnit/src/Main.java
+++ HttpUnit/src/Main.java
@@ -45,7 +45,6 @@ public class Main {
             while (true) {
                 WebResponse response = sc.getResponse(request);
                 System.out.println("Count: " + number++ + response);
-                java.lang.Thread.sleep(200);
             }
         } catch (InterruptedException ex) {
             Logger.getLogger("global").log(Level.SEVERE, null, ex);
